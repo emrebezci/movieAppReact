312457ea884813307f8b25751bd7dc4d71385d69
emrebezci <mrbzcie@gmail.com>
2021-07-15 04:40:26 +0300

    class component to function component

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,23 +1,27 @@
 import React from 'react';
 
 
-class MovieList extends React.Component {
-    render() {
+const MovieList = (props) => {
+    { // HER OBJECT İÇİN MAP FONKSİYONUYLA DÖNEREK CARD YOLLUYORUZ
         return (
             <div className="row">
-                <div className="col-sm-4">
-                    <div className="card mb-3 shadow-sm">
-                        <img src="" className="card-img-top" alt="examplemg" />
-                        <div className="card-body">
-                            <h5 className="card-title">Example movie</h5>
-                            <p className="card-text">Example movie desc</p>
-                            <div className="f-flex justify-content-between align-items-center">
-                                <button type="button" className="btn btn-md btn-outline-danger">Del</button>
-                                <h2><span className="badge badge-info bg-dark">7.5</span></h2>
+                
+                {props.movies.map((movie) =>
+                    <div className="col-sm-4" key={movie.id}>
+                        <div className="card mb-3 shadow-sm">
+                            <img src={movie.imageURL} className="card-img-top" alt="examplemg" />
+                            <div className="card-body">
+                                <h5 className="card-title">{movie.name}</h5>
+                                <p className="card-text">{movie.overwiev}</p>
+                                <div className="f-flex justify-content-between align-items-center">
+                                    <button type="button" className="btn btn-md btn-outline-danger">Del</button>
+                                    <h2><span className="badge badge-info bg-primary">{movie.raiting}</span></h2>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                )}
+
             </div>
         )
     }
